082a06b5b3b92ff10da044a6877cb55052594e82
tendaiwbm <tmbwanda52@gmail.com>
2025-03-12 10:45:22 +0100

    map abbreviated country property values to full names

---
+++ db/translations.py
@@ -0,0 +1,8 @@
+COUNTRY_MAP = {"MWI": "Malawi",
+               "KEN": "Kenya",
+               "UGA": "Uganda",
+               "ETH": "Ethiopia",
+               "PHL": "Philippines",
+               "SSD": "South Sudan",
+               "ZMB": "Zambia"}
+
