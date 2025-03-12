7e21dc88c27bd3527e5c1b327a7c589293c5b5d5
tendaiwbm <tmbwanda52@gmail.com>
2025-03-12 08:29:29 +0100

    user tweak

--- db/data.sh
+++ db/data.sh
@@ -1,4 +1,6 @@
-psql -U tendaiwbm -f sweep-db.sql
-psql -U tendaiwbm -f 01-setup-db.sql
+psql -U $USER -f sweep-db.sql
+psql -U $USER -f 01-setup-db.sql
 python3 02-insert-logs.py
-psql -U tendaiwbm -d ibf-logs -f 03-filter-prod.sql
+psql -U $USER -d ibf-logs -f 03-filter-prod.sql
+psql -U $USER -d ibf-logs -f 04-separate-pages.sql
+psql -U $USER -d ibf-logs -f 050-dashboard-tests.sql
