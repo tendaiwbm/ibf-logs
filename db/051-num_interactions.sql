1c0aab079eee875693f7a2b393e0c5e431aed386
tendaiwbm <tmbwanda52@gmail.com>
2025-03-14 09:38:15 +0100

    refactor function weekly_interactions to also generate number of monthly interactions

--- db/051-num_interactions.sql
+++ db/051-num_interactions.sql
@@ -1,21 +1,30 @@
-CREATE OR REPLACE FUNCTION weekly_interactions()
+CREATE OR REPLACE FUNCTION num_interactions_per_interval()
 RETURNS void AS
 $$
 
 DECLARE
 	sourceTable TEXT := 'production_filtered_dashboard';
-	destTable   TEXT := 'dashboard_weekly_interations';
+	destTable   TEXT := NULL;
 	altText     TEXT := NULL;
 	updText     TEXT := NULL;
 	createText  TEXT := NULL;
 	queryText   TEXT := NULL;
+	interval    RECORD;
 BEGIN
-	altText    := concat('ALTER TABLE ',sourceTable,' ADD COLUMN week_number INTEGER;');
-	updText    := concat('UPDATE ',sourceTable,' SET week_number = (SELECT EXTRACT(''week'' from time_generated));');
-	createText := concat('CREATE TABLE ',destTable,' AS (SELECT count(*),week_number FROM ',sourceTable,' GROUP BY week_number);');
-	queryText  := concat(altText,updText,createText);
-
-	EXECUTE queryText;
+	FOR interval in 
+		SELECT * FROM 
+		(VALUES
+			('week'),
+			('month')
+		) AS t(name)
+	LOOP
+		destTable  := concat('dashboard_',interval.name,'ly_interactions');
+		altText    := concat('ALTER TABLE ',sourceTable,' ADD COLUMN ',interval.name,'_number INTEGER;');
+		updText    := concat('UPDATE ',sourceTable,' SET ',interval.name,'_number = (SELECT EXTRACT(''',interval.name,''' from time_generated));');
+		createText := concat('CREATE TABLE ',destTable,' AS (SELECT count(*),',interval.name,'_number FROM ',sourceTable,' GROUP BY ',interval.name,'_number);');
+		queryText  := concat(altText,updText,createText);
+		EXECUTE queryText;
+	END LOOP;
 END
 $$
 LANGUAGE plpgsql;
@@ -24,5 +33,5 @@ LANGUAGE plpgsql;
 
 DO $$
 BEGIN
-	PERFORM weekly_interactions();
+	PERFORM num_interactions_per_interval();
 END $$
