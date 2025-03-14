32dd9d1448ef0657c3d6e6e74fc0d33dd5c2fac9
tendaiwbm <tmbwanda52@gmail.com>
2025-03-14 07:13:39 +0100

    weekly interactions table

--- db/data.sh
+++ db/data.sh
@@ -4,3 +4,5 @@ python3 02-insert-logs.py
 psql -U $USER -d ibf-logs -f 03-filter-prod.sql
 psql -U $USER -d ibf-logs -f 04-separate-pages.sql
 psql -U $USER -d ibf-logs -f 050-dashboard-tests.sql
+psql -U $USER -d ibf-logs -f 051-num_interactions.sql
+
