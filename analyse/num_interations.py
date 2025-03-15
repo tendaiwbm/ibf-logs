ec14080b1363e0098b2b75675156fdbd6b7d8f83
tendaiwbm <tmbwanda52@gmail.com>
2025-03-15 13:27:41 +0100

    visualise weekly interactions over time

--- analyse/num_interations.py
+++ analyse/num_interations.py
@@ -1,10 +1,11 @@
-import seaborn as sb
-import numpy as np
+from numpy import array,arange
 import matplotlib.pyplot as plt
 from datetime import datetime
 from interface import query_executor
 
 TODAY = datetime.today().strftime('%Y-%m-%d')
+OUTPUT_DIR = "../views/images/"
+
 
 def recursive_backwards_sort(week,max_week_depth,interactions,new_list):
     if week == max_week_depth:
@@ -16,7 +17,7 @@ def recursive_backwards_sort(week,max_week_depth,interactions,new_list):
     return idx+1
     
 def recursive_forward_sort(week,interactions,new_list):
-    if week == 1:
+    if week == 0:
         return new_list.index(0)
     
     idx = recursive_forward_sort(week-1,interactions,new_list)
@@ -24,6 +25,13 @@ def recursive_forward_sort(week,interactions,new_list):
         new_list[idx] = tuple([week,interactions[week]])
     return idx+1
 
+def plot(title,x,y,x_label,x_pad,y_label,y_pad,file_name):
+    plt.plot(x,y,color="r",marker="o",markersize=4,linestyle="--")
+    plt.xlabel(x_label,labelpad=x_pad)
+    plt.ylabel(y_label,labelpad=y_pad)
+    plt.title(title,pad=13)
+    plt.savefig("".join([OUTPUT_DIR,file_name]),dpi=250)
+    return True
 
 def weekly_interactions():
     currentWeekQuery = f"""SELECT EXTRACT('week' from '{TODAY}'::DATE)::INTEGER"""
@@ -35,8 +43,12 @@ def weekly_interactions():
     recursive_backwards_sort(52,52-currentWeek,interactionsDict,sortedInteractions)
     sortedInteractions = sortedInteractions[4:] + [0]*4
     recursive_forward_sort(currentWeek,interactionsDict,sortedInteractions)
-    sortedInteractions = sortedInteractions[:-1] 
-
+    sortedInteractions = array(sortedInteractions[:-1])
+    
+    x = sortedInteractions[:,0]
+    y = sortedInteractions[:,1]
+    xticks = [str(week) for week in x]
+    plot("IBF Dashboard Interactions per Week",xticks,y,"Week Number",9,"Number of Interactions",11,"weekly_interactions.png")
 
 if __name__ == "__main__":
     weekly_interactions()
