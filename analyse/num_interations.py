2c4e83d4f075a75a92f884df936082ecd1ee864a
tendaiwbm <tmbwanda52@gmail.com>
2025-03-15 10:51:10 +0100

    perform backward and forward recursive sorting separately

--- analyse/num_interations.py
+++ analyse/num_interations.py
@@ -6,15 +6,24 @@ from interface import query_executor
 
 TODAY = datetime.today().strftime('%Y-%m-%d')
 
-def recursive_sort_interactions(week,max_week_depth,interactions,new_list):
+def recursive_backwards_sort(week,max_week_depth,interactions,new_list):
     if week == max_week_depth:
         return 0
     
-    idx = recursive_sort_interactions(week-1,max_week_depth,interactions,new_list)
+    idx = recursive_backwards_sort(week-1,max_week_depth,interactions,new_list)
     if week in interactions:
         new_list[idx] = tuple([week,interactions[week]])
     return idx+1
     
+def recursive_forward_sort(week,interactions,new_list):
+    if week == 1:
+        return new_list.index(0)
+    
+    idx = recursive_forward_sort(week-1,interactions,new_list)
+    if week in interactions:
+        new_list[idx] = tuple([week,interactions[week]])
+    return idx+1
+
 
 def weekly_interactions():
     currentWeekQuery = f"""SELECT EXTRACT('week' from '{TODAY}'::DATE)::INTEGER"""
@@ -23,9 +32,10 @@ def weekly_interactions():
     interactions = query_executor(query)
     interactionsDict = {interaction[1]: interaction[0] for interaction in interactions}
     sortedInteractions = [0]*len(interactions)
-    recursive_sort_interactions(52,52-currentWeek,interactionsDict,sortedInteractions)
-    sortedInteractions = sortedInteractions[4:]
-
+    recursive_backwards_sort(52,52-currentWeek,interactionsDict,sortedInteractions)
+    sortedInteractions = sortedInteractions[4:] + [0]*4
+    recursive_forward_sort(currentWeek,interactionsDict,sortedInteractions)
+    sortedInteractions = sortedInteractions[:-1] 
 
 
 if __name__ == "__main__":
