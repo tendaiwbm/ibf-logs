8baf951545c2f0991408f5a591e2dc99bad624a2
tendaiwbm <tmbwanda52@gmail.com>
2025-03-15 10:20:37 +0100

    sort week numbers to account for year change

--- analyse/num_interations.py
+++ analyse/num_interations.py
@@ -6,15 +6,26 @@ from interface import query_executor
 
 TODAY = datetime.today().strftime('%Y-%m-%d')
 
-def sort_interactions(depth,old_list,new_list):
-    if depth == 0:
-        return
-
+def recursive_sort_interactions(week,max_week_depth,interactions,new_list):
+    if week == max_week_depth:
+        return 0
+    
+    idx = recursive_sort_interactions(week-1,max_week_depth,interactions,new_list)
+    if week in interactions:
+        new_list[idx] = tuple([week,interactions[week]])
+    return idx+1
+    
 
 def weekly_interactions():
+    currentWeekQuery = f"""SELECT EXTRACT('week' from '{TODAY}'::DATE)::INTEGER"""
+    currentWeek = query_executor(currentWeekQuery)[0][0]
     query = """SELECT * FROM dashboard_weekly_interactions"""
     interactions = query_executor(query)
-    
+    interactionsDict = {interaction[1]: interaction[0] for interaction in interactions}
+    sortedInteractions = [0]*len(interactions)
+    recursive_sort_interactions(52,52-currentWeek,interactionsDict,sortedInteractions)
+    sortedInteractions = sortedInteractions[4:]
+
 
 
 if __name__ == "__main__":
