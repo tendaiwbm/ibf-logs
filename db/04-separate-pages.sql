3b7caa0baf2b5144971fc6b1a31f481a9ab26e32
tendaiwbm <tmbwanda52@gmail.com>
2025-03-30 07:44:48 +0200

    removed deprecated db dir

--- db/04-separate-pages.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE production_filtered_login AS (
-	SELECT * FROM production_filtered
-	WHERE operation_name = '/login'
-);
-
-CREATE TABLE production_filtered_dashboard AS (
-	SELECT * FROM production_filtered
-	WHERE operation_name = '/'
-);
--- db/data.sh
+++
@@ -1,8 +0,0 @@
-psql -U $USER -f sweep-db.sql
-psql -U $USER -f 01-setup-db.sql
-python3 02-insert-logs.py
-psql -U $USER -d ibf-logs -f 03-filter-prod.sql
-psql -U $USER -d ibf-logs -f 04-separate-pages.sql
-psql -U $USER -d ibf-logs -f 050-dashboard-tests.sql
-psql -U $USER -d ibf-logs -f 051-num_interactions.sql
-
