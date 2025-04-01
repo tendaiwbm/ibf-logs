cf28ce62812f0179885a3a03296c0332d587126d
tendaiwbm <tmbwanda52@gmail.com>
2025-04-01 05:30:36 +0200

    removed redundant statements from main.js

--- server/views/scripts/visits/main.js
+++ server/views/scripts/visits/main.js
@@ -1,11 +1,2 @@
 // global stuff for visits.html
 
-var startDate = document.getElementById("start-date");
-var endDate = document.getElementById("end-date");
-today = new Date();
-max = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + today.getDate();
-startDate.max = max;
-endDate.min = max;
-endDate.max = max
-
-
