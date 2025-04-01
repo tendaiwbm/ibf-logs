0d38fe392e5359b5eaaa06ad40d562c634295cca
tendaiwbm <tmbwanda52@gmail.com>
2025-04-01 05:12:37 +0200

    query LogAnalytics with valid date range from visits.html

--- server/ibf_logs/utils/logs.py
+++ server/ibf_logs/utils/logs.py
@@ -1,19 +1,17 @@
-from datetime import datetime
-from pytz import timezone
+import os
+from datetime import datetime,timezone
 from azure.core.exceptions import HttpResponseError
 from azure.identity import DefaultAzureCredential
 from azure.monitor.query import LogsQueryClient, LogsQueryStatus, LogsBatchQuery
 import pandas as pd
-import os
 
 credential = DefaultAzureCredential()
 client = LogsQueryClient(credential)
 
 def fetch_logs(start=None,end=None):
     requests = [LogsBatchQuery(query="AppEvents",
-                               timespan=(datetime(2021, 6, 2, tzinfo=timezone("UTC")),datetime(2021, 6, 5, tzinfo=timezone("UTC"))),
+                               timespan=(datetime(*start,tzinfo=timezone.utc),datetime(*end,tzinfo=timezone.utc)),
                                workspace_id=os.environ.get("LOGS_WORKSPACE_ID"))]
-
     try:
         responses = client.query_batch(requests)
         for response in responses:
@@ -25,7 +23,6 @@ def fetch_logs(start=None,end=None):
 
             for table in data:
                 df = pd.DataFrame(data=table.rows, columns=table.columns)
-        print(df)
         return df
 
     except HttpResponseError:
