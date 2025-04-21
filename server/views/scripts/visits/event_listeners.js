71f09e7fcc49d5263cf55ee1d828124a4509aca5
tendaiwbm <tmbwanda52@gmail.com>
2025-04-21 07:43:40 +0200

    cleanup: removed deprecated scripts

--- server/views/scripts/visits/event_listeners.js
+++
@@ -1,3 +0,0 @@
-// event listeners for visits.html
-var button = document.getElementById("generate-logs-table");
-// button.addEventListener("click",Table.invoke_generate_table);
--- server/views/scripts/visits/response.js
+++
@@ -1,6 +0,0 @@
-// response handlers for visits
-
-function generate_table_response(event,response) {
-	const x = JSON.parse(response);
-	console.log(Object.keys(x).length,x);
-}
