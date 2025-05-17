8534970d901e7b229ddd90229a05aed2921cff24
tendaiwbm <tmbwanda52@gmail.com>
2025-05-17 07:26:36 +0200

    feat: renamed constants to query_builder

--- server/ibf_logs/utils/constants.py
+++
@@ -1,8 +0,0 @@
-PAGINATION_FILTER = " | where TimeGenerated {} todatetime('{} 00:00:00')" 
-
-PAGINATION_DIRECTION = {
-                        "left": "<",
-                        "right": ">"
-                       }
-
-
