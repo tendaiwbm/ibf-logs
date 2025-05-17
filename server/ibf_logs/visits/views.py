1c429132078e70cccef288b4a62c12ea7229a4be
tendaiwbm <tmbwanda52@gmail.com>
2025-05-17 07:24:24 +0200

    feat: query_builder module with prepared KQL statements

--- server/ibf_logs/visits/views.py
+++ server/ibf_logs/visits/views.py
@@ -4,11 +4,12 @@ from django.conf import settings
 
 from utils.data_validation import parse_date
 from utils.logs import fetch_logs
+from utils.query_builder import TABLE_NAME, ORDER_BY, PAGINATION_FILTER, PAGINATION_DIRECTION, FORMAT_QUERY
 
 
 def visits(request):
-    dateInterval = parse_date(request.GET["date"])
-    logsDF = fetch_logs(dateInterval)
+    dateInterval = parse_date(request.GET["date"]) 
+    logsDF = fetch_logs(dateInterval,TABLE_NAME)
     logsDF["Properties"] = [loads(string) for string in logsDF["Properties"]]
 
     RESPONSE = {
@@ -20,6 +21,11 @@ def visits(request):
 
 def get_page(request):
     dateInterval = parse_date(request.GET["date"])
+    
+    selectionCondition = PAGINATION_FILTER.format(PAGINATION_DIRECTION[request.GET["dir"]],request.GET["predicate"])
+    orderByClause = ORDER_BY.format("TimeGenerated","desc")
+    newPageQuery = FORMAT_QUERY([TABLE_NAME,selectionCondition,orderByClause])
+    
     logsDF = fetch_logs(dateInterval)
     logsDF["Properties"] = [loads(string) for string in logsDF["Properties"]]
 
