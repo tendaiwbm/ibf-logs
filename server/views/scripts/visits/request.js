a0794bf27c18340f7376dbee129bc019be0cf246
tendaiwbm <tmbwanda52@gmail.com>
2025-05-21 07:26:09 +0200

    feat: query output validation - detect 0 zero records returned, & pass message to FE

--- server/views/scripts/visits/request.js
+++ server/views/scripts/visits/request.js
@@ -1,11 +1,11 @@
 const BASE = "http://ibf.logs:8082/api/visits";
 const PAGE_ROUTE = "/page";
 
-function request(url,response_handler) {
+function request(url,response_inspector,response_handler) {
 	var requestObj = new XMLHttpRequest();
 	requestObj.onreadystatechange = function(event) {
 										if (requestObj.readyState === 4 && requestObj.status === 200) {
-											response_handler(event,requestObj.response);
+											response_inspector(event,response_handler,requestObj.response);
 										}
 									};
 	requestObj.open("GET",url,true);
