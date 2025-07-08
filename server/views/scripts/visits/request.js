14834c5efaa353be5c57bf89ef3de11f4e769e92
tendaiwbm <tmbwanda52@gmail.com>
2025-07-08 08:49:29 +0200

    refactor: removing obsolete build_url function

--- server/views/scripts/visits/request.js
+++ server/views/scripts/visits/request.js
@@ -1,7 +1,3 @@
-const BASE = "http://ibf.logs:8082/api/visits";
-const PAGE_ROUTE = "/page";
-const UNIQUE_COLUMN_VALUES_ROUTE = "/unique-values";
-
 function request(url,response_inspector,response_handler) {
 	var requestObj = new XMLHttpRequest();
 	requestObj.onreadystatechange = function(event) {
@@ -12,34 +8,3 @@ function request(url,response_inspector,response_handler) {
 	requestObj.open("GET",url,true);
 	requestObj.send();
 };
-
-function build_url(params) {
-	var queryString = "";
-	const endpoint = params["endpoint"];
-	const queryObject = params["query"];
-	const sortObject = params["sort"];
-
-    for (var key in queryObject) {
-        const parameter = `&${key}=${queryObject[key]}`;
-        queryString = queryString + parameter;
-    }
-
-    var sortString = "";
-    for (var key in sortObject) {
-        var columnSort = [key,sortObject[key]].join("-");
-        if (sortString === "") {
-        	sortString = columnSort;
-        }
-        else {
-        	sortString = [sortString,columnSort].join(",");
-        }
-    }
-    queryString = queryString + `&sort=${sortString}`;
-
-    if (PageState["sortingActive"]) {
-    	queryString = queryString + `&pageNumber=${params["pageNumber"]}`;
-    }
-    
-    const url = `${BASE}${endpoint}?v=table${queryString}`;
-    return url;
-}
