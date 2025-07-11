23d3338bb4b0f21568a06973c26c558ce7985619
tendaiwbm <tmbwanda52@gmail.com>
2025-07-11 07:20:19 +0200

    fix: date validation - validate_date_input (FE) return 'null' when no range specified and parse_date (BE) creates date interval between first recorded date and today

--- server/ibf_logs/utils/data_validation.py
+++ server/ibf_logs/utils/data_validation.py
@@ -3,11 +3,13 @@ from datetime import datetime
 
 def parse_date(date_interval):
     if date_interval == "null":
-        dateInterval = None
-    else:
-        dateInterval = tuple([int(datePart) for datePart in date.split("-")] for date in tuple(date_interval.split(",")))
-        dateInterval = tuple([datetime(*dateInterval[0],hour=0,minute=0,second=0),datetime(*dateInterval[1],hour=23,minute=59,second=59)])
-
+        intervalLowerBound = "2024-11-01"
+        intervalUpperBound = datetime.strftime(datetime.today(),"%Y-%m-%d")
+        date_interval = ",".join([intervalLowerBound,intervalUpperBound])
+    
+    dateInterval = tuple([int(datePart) for datePart in date.split("-")] for date in tuple(date_interval.split(",")))
+    dateInterval = tuple([datetime(*dateInterval[0],hour=0,minute=0,second=0),datetime(*dateInterval[1],hour=23,minute=59,second=59)])
+    
     return dateInterval
 
 def parse_column_name(column_name):
