eef48d167b9c1eb03dd948acd93411daadc0a9cc
tendaiwbm <tmbwanda52@gmail.com>
2025-07-15 06:21:07 +0200

    feat: query output - by default check if df has > 1 column and return df[ViewColumns]

--- server/ibf_logs/utils/query_output.py
+++ server/ibf_logs/utils/query_output.py
@@ -8,9 +8,9 @@ def query_output_inspector(query_executor):
         queryResult = query_executor(arg1,arg2)
         if isinstance(queryResult,DataFrame):
             if len(queryResult) > 0:
-                if len(queryResult.keys()) == 1:
-                    return queryResult
-                return queryResult[ViewColumns]
+                if len(queryResult.keys()) > 1:
+                    return queryResult[ViewColumns]
+                return queryResult
             else:
                 return {"num_records": 0}
         elif isinstance(queryResult,HttpResponsError):
