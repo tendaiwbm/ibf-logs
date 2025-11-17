8ad4e0ff42c4d010c6b9b822df99218ede14f822
tendaiwbm <tmbwanda52@gmail.com>
2025-11-17 06:22:35 +0100

    migrated plot_weekly_users to visits/scripts/weekly_users

---
+++ server/views/visits/scripts/weekly_users/plot.js
@@ -0,0 +1,22 @@
+// import graph config
+// import LineGraph
+
+
+function request_weekly_users() {
+	let url = "http://ibf.logs:8082/api/graph/visits/users-weekly";
+	var requestObj = new XMLHttpRequest();
+	requestObj.onreadystatechange = function(event) {
+										if (requestObj.readyState === 4 && requestObj.status === 200) {
+											plot_weekly_users(event,requestObj.response);
+										}
+									};
+	requestObj.open("GET",url,true);
+	requestObj.send();
+}
+
+function plot_weekly_users(event,data) {
+		let interactions = JSON.parse(data).data;
+		const graph = new LineGraph(interactions,graphConfig);
+		graph.generate();
+		 
+}
