c4e6b06b29478aef7db7a81cf62e472772d3616b
tendaiwbm <tmbwanda52@gmail.com>
2025-11-22 09:23:09 +0100

    grouping related util functions in new utils directory

---
+++ server/views/shared/scripts/utils/data_validation.js
@@ -0,0 +1,19 @@
+export function validate_date_input(date_object) { 
+    if ((date_object["startDate"] === "" & date_object["endDate"] != "") || 
+       (date_object["endDate"] === "" & date_object["startDate"] != "")) {
+        alert("Start and end dates must either both be populated or null."); 
+        throw new Error("Start and end dates must either both be populated or null.");
+    }
+    else {
+        if (date_object["startDate"] === "" & date_object["endDate"] === "") { 
+            const dateString = "null";
+            const paramDict = {"dateRange": dateString};
+            update_state(TableState,paramDict);
+        }
+        else { 
+            const dateString = `${date_object["startDate"]},${date_object["endDate"]}`; 
+            const paramDict = {"dateRange": dateString};
+            update_state(TableState,paramDict);
+        }
+    }
+}
